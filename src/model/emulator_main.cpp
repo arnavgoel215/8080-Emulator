13c5637e160301886cd774a80ac306fe68e95757
Sergio Chavarria <chavarse@oregonstate.edu>
2025-07-07 23:11:12 -0600

    Emulator build sample.

---
+++ src/model/emulator_main.cpp
@@ -0,0 +1,28 @@
+/**********************************************************
+ * @file emulator_main.cpp
+ * 
+ * @brief Example file for developing emulator module.
+ * 
+ * @author: <TODO: ADD AUTHOR>
+ * 
+ *********************************************************/
+
+/***************** Include files. ***********************/
+#include <iostream>
+#include "emulator.hpp"
+
+/***************** Macros and defines. ***********************/
+
+/***************** Namespaces. ***********************/
+
+/***************** Local Classes. ***********************/
+
+/***************** Local Functions. ***********************/
+
+int main (int argc, char * argv[])
+{
+    std::cout << "REPLACE ME! I'M THE EMULATOR!" << std::endl;
+    return 0;
+}
+
+/***************** Global Class Functions. ***********************/
