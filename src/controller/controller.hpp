cc71018b954b34d374c9dff1f5e2acddca3d151b
Sergio Chavarria <chavarse@oregonstate.edu>
2025-07-07 23:10:56 -0600

    Controller build sample.

---
+++ src/controller/controller.hpp
@@ -0,0 +1,23 @@
+/**********************************************************
+ * @file controller.hpp
+ * 
+ * @brief <TODO: ADD DESCRIPTION>
+ * 
+ * @author: <TODO: ADD AUTHOR>
+ * 
+ *********************************************************/
+#ifndef CONTROLLER_HPP_
+#define CONTROLLER_HPP_
+
+/***************** Include files. ***********************/
+
+/***************** Macros and defines. ***********************/
+
+/***************** Namespaces. ***********************/
+
+/***************** Global Classes. ***********************/
+
+/***************** Global Functions. ***********************/
+
+
+#endif /* CONTROLLER_HPP_ */
