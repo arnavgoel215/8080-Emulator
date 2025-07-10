fa7e6c8692bd0eb6a1faf8822f0577de944ae3c8
hyattj <hyattj@oregonstate.edu>
2025-07-10 18:03:42 -0400

    initial controller api

--- src/controller/controller.hpp
+++ src/controller/controller.hpp
@@ -1,23 +1,70 @@
 /**********************************************************
  * @file controller.hpp
  * 
- * @brief <TODO: ADD DESCRIPTION>
+ * @brief Public interface for the Controller.
+ *        This class connects user actions from the View to the Model (Emulator)
+ *        and drives the main emulation loop.
  * 
- * @author: <TODO: ADD AUTHOR>
+ * @author Jesse
  * 
  *********************************************************/
 #ifndef CONTROLLER_HPP_
 #define CONTROLLER_HPP_
 
 /***************** Include files. ***********************/
-
-/***************** Macros and defines. ***********************/
-
-/***************** Namespaces. ***********************/
+#include <string>
+#include "../model/emulator.hpp" // Needs to know about the Emulator's public interface
 
 /***************** Global Classes. ***********************/
+// Forward-declaration of the View's main window class to avoid including Qt headers here.
+// This breaks the circular dependency between Controller and View.
+class MainView; 
 
-/***************** Global Functions. ***********************/
+/**
+ * @brief The Controller class, responsible for application logic.
+ */
+class Controller
+{
+public:
+    /**
+     * @brief Constructs the Controller.
+     * @param model A pointer to the Emulator instance.
+     * @param view A pointer to the MainView instance.
+     */
+    Controller(Emulator* model, MainView* view);
 
+    // --- User Action Handlers (called by the View) ---
+
+    /**
+     * @brief Handles the user request to load a ROM file.
+     * @param romFilePath The absolute path to the ROM file.
+     */
+    void on_loadROM(const std::string& romFilePath);
+
+    /**
+     * @brief Handles the user request to toggle the emulation (Start/Pause).
+     */
+    void on_toggleRun();
+
+    /**
+     * @brief Handles the user request to reset the emulation.
+     */
+    void on_reset();
+
+    /**
+     * @brief Handles a keyboard event from the View.
+     * @param key An integer identifying the key (e.g., from Qt::Key).
+     * @param isPressed The state of the key (true for pressed, false for released).
+     */
+    void on_keyEvent(int key, bool isPressed);
+
+    // --- Main Emulation Loop ---
+
+    /**
+     * @brief Executes a single frame's worth of emulation and updates the view.
+     *        This function should be called repeatedly by a timer in the View (e.g., 60Hz).
+     */
+    void runFrame();
+};
 
 #endif /* CONTROLLER_HPP_ */
